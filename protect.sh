68c010d400bc9a737f6edde64f4ed68d54e79966
ganghe74 <ganghe74@kookmin.ac.kr>
2019-02-05 12:00:00 -0800

    environment setting

--- protect.sh
+++ protect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-date >> /home/ubuntu/project/protectgrass/history.log
+date >> /home/pi/workspace/protectgrass/history.log
 
-cd /home/ubuntu/project/protectgrass/
+cd /home/pi/workspace/protectgrass/
 
-/usr/bin/git add . >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git add . >> /home/pi/workspace/protectgrass/history.log
 
-/usr/bin/git commit -m "protected grass" >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git commit -m "protected grass" >> /home/pi/workspace/protectgrass/history.log
 
-/usr/bin/git push origin master >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git push origin master >> /home/pi/workspace/protectgrass/history.log
 
